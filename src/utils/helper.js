15941fe5553cc6a6d54550badb124914ed074b75
jatin-rajput563 <jkt3298@gmail.com>
2025-06-21 10:32:47 +0530

    worldgame-section-complete

---
+++ src/utils/helper.js
@@ -0,0 +1,22 @@
+import swiperImgOne from "../assets/images/png/slider-img-1.png";
+import swiperImgTwo from "../assets/images/png/slider-img-2.png";
+import swiperImgThree from "../assets/images/png/slider-img-3.png";
+
+export const SLIDER_DATA = [
+    {
+        image: swiperImgOne,
+        text: "Explore the World Around You",
+    },
+    {
+        image: swiperImgTwo,
+        text: "Discover New Adventures",
+    },
+    {
+        image: swiperImgThree,
+        text: "Connect and Play Together",
+    },
+    {
+        image: swiperImgTwo,
+        text: "Challenge Your Mind",
+    },
+];
