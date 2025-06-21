115e4ec95c6958b1c3ffa6c2dc8ac83c7165ec22
jatin-rajput563 <jkt3298@gmail.com>
2025-06-21 12:33:56 +0530

    adding-routs

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ import WorldGame from "./components/WorldGame";
 function App() {
   return (
     <Router>
-      <Event />
-      <WorldGame />
+      <nav className="p-4 bg-gray-200 flex gap-4">
+        <Link to="/event">Event</Link>
+        <Link to="/worldgame">World Game</Link>
+        <Link to="/billie">Billie</Link>
+      </nav>
+
       <Routes>
         <Route path="/event" element={<Event />} />
-        <Route path="/world-game" element={<WorldGame />} />
+        <Route path="/worldgame" element={<WorldGame />} />
         <Route path="/billie/:nameParam?" element={<Billie />} />
       </Routes>
     </Router>
